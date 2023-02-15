f30752fd4f84a2c72be9659a0d0bb4869b256645
suparuu <kkwoo97@naver.com>
2023-02-15 18:14:15 +0900

    2023-02-15

--- next-test/pages/api/hello.js
+++ next-test/pages/api/hello.js
@@ -1,5 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+let fs = require('fs')
+let db = require('data/db.json')
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const {method,body} = req;
+  
+  switch(method){
+    case 'GET': dataGet(); break;
+    case 'POST': dataCreate(); break;
+    case 'PUT': dataUpdate(); break;
+    case 'DELETE': dataDelete(); break;
+  }
+  console.log(method)
+  function dataGet(){
+    res.status(200).json(db)
+  }
+  function dataCreate(){
+    db.push(body);
+    saveData();
+  }
+  function dataUpdate() {
+    let user = db.find(obj => obj.id == body.id);
+    Object.assign(user, body)
+    saveData();
+  }
+  function dataDelete() {
+    db = db.filter(obj => obj.id !== body);
+    saveData();
+  }
+
+  function saveData() {
+    fs.writeFileSync('data/db.json', JSON.stringify(db));
+    res.status(200).json(db)
+  }
+
+
+
 }
